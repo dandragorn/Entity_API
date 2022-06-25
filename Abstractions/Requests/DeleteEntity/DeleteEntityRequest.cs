6e5ed242117e0496cf08b074c78bc569f624eea7
danperunov <danperunov@gmail.com>
2022-06-25 18:07:28 +0300

    Comment

--- Abstractions/Requests/DeleteEntity/DeleteEntityRequest.cs
+++ Abstractions/Requests/DeleteEntity/DeleteEntityRequest.cs
@@ -7,5 +7,6 @@ public class DeleteEntityRequest : IRequest<DeleteEntityResponse>
     //plan to delete some entities
     public DeleteEntityRequest(int id) => Id = id;
     
+    //commenting stuff
     public int Id { get; set; }
 }
